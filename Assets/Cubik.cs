5574d71c5c023935efc272d8d81559215863e2ba
latyshev fedor <latyshev.fedor@gmail.com>
2021-02-23 05:03:39 +0200

    initial game upload

---
+++ Assets/Cubik.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cubik : MonoBehaviour
+{
+    public GameObject myPrefab;
+    public Material newMaterialRef;
+
+
+    // This script will simply instantiate the Prefab when the game starts.
+    void Start()
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            for(int j = 0; j < 3; j++)
+            {
+                for(int k = 0; k < 3; k++)
+                {
+                    GameObject go = Instantiate(myPrefab, new Vector3(i*2f, j*2f, k*1.4f), Quaternion.identity) as GameObject;
+                    go.AddComponent<piece>();
+                    Renderer rend = go.AddComponent<Renderer>();
+                    if (rend != null){
+                        rend.material = newMaterialRef;
+                    }
+                }
+            }
+        }
+        // Instantiate at position (0, 0, 0) and zero rotation.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/piece.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class piece : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(this.gameObject.transform.position, Vector3.up, 20 * Time.deltaTime);
+    }
+}
